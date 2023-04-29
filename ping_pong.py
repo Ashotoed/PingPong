2dbe7d1aa60b56ae1fe4b366c122d8a01a325825
Ashotoed <132129403+Ashotoed@users.noreply.github.com>
2023-04-29 13:54:57 +0500

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -13,7 +13,6 @@ class GameSprite(sprite.Sprite):
    def reset(self):
        window.blit(self.image, (self.rect.x, self.rect.y))
 
-
 class Player(GameSprite):
    def update_r(self):
        keys = key.get_pressed()
@@ -40,5 +39,10 @@ clock = time.Clock()
 FPS = 60
 
 racket1 = Player('rocket.png', 30, 200, 4, 50, 150)
-racket2 = Player('rocket.png', 30, 200, 4, 50, 150)
+racket2 = Player('rocket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
